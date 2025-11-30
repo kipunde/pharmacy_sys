18548111739eb195359b7412d1c4cbbdea5e2b07
Dullayo Kipunde <abdallah.kipunde@gmail.com>
2025-11-30 07:39:03 +0300

    show password

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -3,26 +3,40 @@
 @section('content')
 <h1>Pharmacy System</h1>
 <p class="account-subtitle">Login Panel</p>
+
 @if (session('login_error'))
-<x-alerts.danger :error="session('login_error')" />
+    <x-alerts.danger :error="session('login_error')" />
 @endif
+
 <!-- Form -->
-<form action="{{route('login')}}" method="post">
-	@csrf
-	<div class="form-group">
-		<input class="form-control" name="email" type="text" placeholder="Email">
-	</div>
-	<div class="form-group">
-		<input class="form-control" name="password" type="password" placeholder="Password">
-	</div>
-	<div class="form-group">
-		<button class="btn btn-success btn-block" type="submit">Login</button>
-	</div>
+<form action="{{ route('login') }}" method="post">
+    @csrf
+    <div class="form-group" style="position:relative;">
+        <input class="form-control" name="email" type="text" placeholder="Email">
+    </div>
+    <div class="form-group" style="position:relative;">
+        <input class="form-control" id="password" name="password" type="password" placeholder="Password">
+        <span class="toggle-password" onclick="togglePassword('password')" 
+              style="position:absolute; right:10px; top:50%; transform:translateY(-50%); cursor:pointer;">
+            👁
+        </span>
+    </div>
+    <div class="form-group">
+        <button class="btn btn-success btn-block" type="submit">Login</button>
+    </div>
 </form>
 <!-- /Form -->
 
-<!-- Visit codeastro.com for more projects -->
+<div class="text-center forgotpass"><a href="{{ route('password.request') }}">Forgot Password?</a></div>
 
-<div class="text-center forgotpass"><a href="{{route('password.request')}}">Forgot Password?</a></div>
-<!-- <div class="text-center dont-have">Don’t have an account? <a href="{{route('register')}}">Register</a></div> -->
+<script>
+    function togglePassword(id) {
+        const input = document.getElementById(id);
+        if (input.type === "password") {
+            input.type = "text";
+        } else {
+            input.type = "password";
+        }
+    }
+</script>
 @endsection
